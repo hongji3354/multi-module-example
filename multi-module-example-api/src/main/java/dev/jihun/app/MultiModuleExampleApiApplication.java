aaedddbdd34fbfb866de9cf4c2e7939bdbb3d841
JiHun Hong <hongji3354@gmail.com>
2022-09-20 23:06:33 +0900

    feat : Multi Module로 변경

--- multi-module-example-api/src/main/java/dev/jihun/app/MultiModuleExampleApiApplication.java
+++ multi-module-example-api/src/main/java/dev/jihun/app/MultiModuleExampleApiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MultiModuleExampleApplication {
+public class MultiModuleExampleApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MultiModuleExampleApplication.class, args);
+		SpringApplication.run(MultiModuleExampleApiApplication.class, args);
 	}
 
 }
--- multi-module-example-api/src/test/java/dev/jihun/app/MultiModuleExampleApiApplicationTests.java
+++ multi-module-example-api/src/test/java/dev/jihun/app/MultiModuleExampleApiApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MultiModuleExampleApplicationTests {
+class MultiModuleExampleApiApplicationTests {
 
 	@Test
 	void contextLoads() {
