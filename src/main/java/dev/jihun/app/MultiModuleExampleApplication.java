6b5b35e7ee6aba67c132235048fa5aac6f6e0a69
JiHun Hong <hongji3354@gmail.com>
2022-09-20 22:49:52 +0900

    init commit

---
+++ src/main/java/dev/jihun/app/MultiModuleExampleApplication.java
@@ -0,0 +1,13 @@
+package dev.jihun.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiModuleExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiModuleExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/jihun/app/MultiModuleExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jihun.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiModuleExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
