47670b476d6612e4e9d1a4c132bdc7e7b3c8db69
JiHun Hong <hongji3354@gmail.com>
2022-09-21 00:55:33 +0900

    feat: db 모듈 datasource 정보 추가

--- storage/db-module/src/main/java/dev/jihun/app/storage/config/MainDataSourceConfig.java
+++ storage/db-module/src/main/java/dev/jihun/app/storage/config/MainDataSourceConfig.java
@@ -1,4 +1,22 @@
 package dev.jihun.app.storage.config;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class MainDataSourceConfig {
+
+    @Bean
+    @ConfigurationProperties(prefix = "dev.jihun.datasource.main")
+    public HikariConfig mainHikariConfig() {
+        return new HikariConfig();
+    }
+
+    @Bean
+    public HikariDataSource mainDataSource() {
+        return new HikariDataSource(mainHikariConfig());
+    }
 }
--- storage/db-module/src/main/java/dev/jihun/app/storage/config/MainJpaConfig.java
+++ storage/db-module/src/main/java/dev/jihun/app/storage/config/MainJpaConfig.java
@@ -1,4 +1,13 @@
 package dev.jihun.app.storage.config;
 
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+@EntityScan(basePackages = "dev.jihun.app.storage")
+@EnableJpaRepositories(basePackages = "dev.jihun.app.storage")
 public class MainJpaConfig {
 }
